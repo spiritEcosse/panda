57d872e3fdcfa3ca35b39f7363c701a5740082e2
igor <shevchenkcoigor@gmail.com>
2019-08-16 23:20:49 +0300

    add java 1.8 , pythonn 3.6.9, add install search_engine

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
+search_engine: ./run_search_engine.sh
 release: python manage.py migrate
 web: gunicorn config.wsgi:application
 worker: celery worker --app=config.celery_app --loglevel=info
---
+++ run_search_engine.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.6/elasticsearch-2.4.6.tar.gz
+tar -xvf elasticsearch-2.4.6.tar.gz
+cd elasticsearch-2.4.6/bin
+./elasticsearch
