c6a73629d3b39b0d58dc625d0259fd878f5ed69b
igor <shevchenkcoigor@gmail.com>
2019-08-19 16:35:31 +0300

    add commands for install elasticsearch on server

--- run_search_engine.sh
+++ run_search_engine.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.6/elasticsearch-2.4.6.tar.gz
-tar -xvf elasticsearch-2.4.6.tar.gz
-cd elasticsearch-2.4.6/bin
-./elasticsearch &
+curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.4.6/elasticsearch-2.4.6.rpm
+rpm -ivh elasticsearch-2.4.6.rpm
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl start elasticsearch.service
