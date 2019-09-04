c34ae30fb47743980b63213ef41345b888dbe139
igor <shevchenkcoigor@gmail.com>
2019-09-04 17:17:13 +0300

    add fields on MessageSerializer and test it

--- panda/telegram_bot/exceptions.py
+++ panda/telegram_bot/exceptions.py
@@ -1,6 +1,8 @@
 from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import ValidationError
-
+from django.forms import ValidationError
 
 class MessageException(ValidationError):
     default_detail = _("Wrong field price.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self.default_detail, *args, **kwargs)
