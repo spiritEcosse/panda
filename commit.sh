60d32c3b69d759567247b7374c2b031d49e403ca
igor <shevchenkcoigor@gmail.com>
2019-09-20 16:45:20 +0300

    fix HTTP ERROR 403 jenkins

--- commit.sh
+++ commit.sh
@@ -2,5 +2,5 @@
 
 if [[ `git rev-parse --abbrev-ref HEAD` == 'master' ]]; then
 	echo "begin deploy, show details in you ci";
-	curl -X POST http://127.0.0.1:8094/job/panda/build?token=hRvyQqWEkbPUQrWwskihxmcBWirNFhnwdUITxhpJQbRjuUIKYPILhYQuVRegKzzN --user "igor:1111" -H "`wget -q --auth-no-challenge --user igor --password 1111 --output-document -H 'http://127.0.0.1:8094/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)`";
+	curl -X POST http://127.0.0.1:8094/job/panda/build?token=hRvyQqWEkbPUQrWwskihxmcBWirNFhnwdUITxhpJQbRjuUIKYPILhYQuVRegKzzN --user "igor:1111" -H "`wget -q --auth-no-challenge --user igor --password 1111 --output-document - 'http://127.0.0.1:8094/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)`";
 fi;
